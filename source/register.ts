a579181b5191da7425c50a5af2c897c41d998a52
Ivan Reese <ivanreese@gmail.com>
2024-12-16 22:04:22 -0700

    ∆

--- source/register.ts
+++ source/register.ts
@@ -5,7 +5,6 @@ const formStructure: FormStructure = {
     fullName: { type: "text", label: "Full Name" },
     preferredName: { type: "text", label: "What would you like us to call you?" },
     pronouns: { type: "text", label: "Preferred Pronouns" },
-    email: { type: "email", label: "Email" },
     phone: { type: "tel", label: "Phone" },
     firstAider: {
       type: "radio",
@@ -60,7 +59,7 @@ const formStructure: FormStructure = {
       type: "radio",
       label: "Where are you planning to stay? (Note: tenting space is very limited.)",
       options: {
-        tenting: "Tenting in the Greenhouse ($30 for the weekend)",
+        tenting: "Tenting in the Greenhouse ($30 for the weekend, billed separately)",
         hotel: "Hotel on my own",
         near: "I live nearby",
         self: "Only coming one day"
@@ -103,7 +102,15 @@ const formStructure: FormStructure = {
       }
     }
   },
-  Workshops: await fetch("workshops.json").then((r) => r.json())
+  Workshops: (await fetch("workshops.json").then((r) => r.json())) as Workshops
+}
+
+const costs = {
+  fridayLunch: { is: true, cost: 25 },
+  fridaySupper: { is: true, cost: 30 },
+  saturdayLunch: { is: true, cost: 25 },
+  saturdaySupper: { is: true, cost: 30 },
+  sundayLunch: { is: true, cost: 25 }
 }
 
 type FormStructure = Record<SectionName, SectionFields | Workshops>
@@ -155,6 +162,20 @@ function generateForm(structure: FormStructure, data = {}) {
       }
     }
   }
+
+  const submit = tag("input", form)
+  submit.type = "submit"
+  submit.onclick = async (e) => {
+    e.preventDefault()
+    submit.disabled = true
+    const res = await fetchJson("https://spiralganglion-weaving.web.val.run/submit", { id })
+    if (res.err) {
+      console.log(res)
+      // Show a modal error with a "reload" button?
+    } else if (res.paymentLink) {
+      window.location.href = res.paymentLink
+    }
+  }
 }
 
 let schedule: HTMLElement
@@ -179,15 +200,24 @@ function makeWorkshops(form: HTMLFormElement, workshops: Workshops, data = {}) {
 
   for (let id in workshops) {
     let details = workshops[id]
+    let rem = remaining[id]
 
     let label = tag("label", elm)
-    label.className = "workshop-token"
+    label.className = rem > 0 ? "workshop-token" : "workshop-token sold-out"
+
+    // TODO: What do we do if someone selected this workshop when it was available,
+    // we saved this state, but they didn't finish registering, then they came back later
+    // and the workshop is now sold out?
+    // We ought to deselect it and save that to the server… but how do we tell if their
+    // registration is finalized or not?
+    // We probably need some state to indicate that a registration is finalized,
+    // but still let people change the fields that don't affect payment or mess up our records.
 
     let cb = tag("input", label)
     rows.push({ id, cb, details })
-    cb.checked = data[id]
+    cb.checked = data[id as keyof typeof data]
     cb.type = "checkbox"
-    cb.addEventListener("change", click(id, cb, details, rows))
+    if (rem > 0) cb.addEventListener("change", toggleWorkshop(id, cb, details, rows))
 
     let img = tag("img", label)
     img.src = "/photos/" + details.image
@@ -203,7 +233,7 @@ function makeWorkshops(form: HTMLFormElement, workshops: Workshops, data = {}) {
     tag("span", meta, "$" + details.totalCost)
     tag("span", meta, details.scheduleDesc)
 
-    let spots = tag("span", right, details.limit + " spots open")
+    let spots = tag("span", right, rem == 1 ? rem + " spot open" : rem > 0 ? rem + " spots open" : "sold out")
     spots.className = "spots"
   }
 
@@ -228,32 +258,34 @@ function updateSchedule(rows: Row[]) {
     let d: Row | null = filteredRows[0]
     let tr = tag("tr", table)
     tag("td", tr, time)
-    let title = tag("td", tr, d?.details?.title ?? "Nothing selected")
+    let title = tag("td", tr, d?.details?.title ?? "Free time")
     if (d == null) title.className = "nuttin"
   }
 }
 
-const click = (myId: WorkshopId, cb: HTMLInputElement, myDetails: WorkshopDetails, rows: Row[]) => (e: Event) => {
-  // Save the new state of this checkbox
-  saveValue(myId, cb.checked)
+const toggleWorkshop =
+  (myId: WorkshopId, cb: HTMLInputElement, myDetails: WorkshopDetails, rows: Row[]) => (e: Event) => {
+    // Save the new state of this checkbox
+    data[myId] = cb.checked
 
-  if (cb.checked) {
-    // Clear any checked checkboxes that conflict with the newly selected one
-    for (const { id, cb, details } of rows) {
-      if (id != myId) {
-        for (let slot of myDetails.scheduleSlots) {
-          if (details.scheduleSlots.includes(slot) && cb.checked) {
-            cb.checked = false
-            saveValue(id, false)
+    if (cb.checked) {
+      // Clear any checked checkboxes that conflict with the newly selected one
+      for (const { id, cb, details } of rows) {
+        if (id != myId) {
+          for (let slot of myDetails.scheduleSlots) {
+            if (details.scheduleSlots.includes(slot) && cb.checked) {
+              cb.checked = false
+              data[id] = false
+            }
           }
         }
       }
     }
-  }
 
-  updateRows(rows)
-  updateSchedule(rows)
-}
+    saveData()
+    updateRows(rows)
+    updateSchedule(rows)
+  }
 
 function updateRows(rows: Row[]) {
   for (const { cb } of rows) cb.className = ""
@@ -327,7 +359,7 @@ function makeRadio(form: HTMLFormElement, name: FieldName, field: RadioField, da
 function makeCheckbox(form: HTMLFormElement, name: FieldName, field: CheckboxField, data: any) {
   makeSelection("checkbox", form, name, field, (input, key) => {
     if (data[key]) input.checked = true
-    input.addEventListener("change", () => saveValue(name, input.checked))
+    input.addEventListener("change", () => saveValue(key, input.checked))
   })
 }
 
@@ -355,12 +387,53 @@ function makeSelection(
 }
 
 function saveValue(name: string, value: any) {
-  console.log("save", name, value)
-  if (id != null) fetchJson("https://spiralganglion-weaving.web.val.run/save", { id, name, value })
+  data[name] = value
+  saveData()
+}
+
+function saveData() {
+  if (id != null) fetchJson("https://spiralganglion-weaving.web.val.run/save", { id, data }).then((x) => console.log(x))
+  updateSubtotal()
+}
+
+function updateSubtotal() {
+  let subtotal = 0
+  subtotalElm.innerHTML = ""
+
+  // Registration fee
+  subtotal += 25
+  let tr = tag("tr", subtotalElm)
+  tag("td", tr, "Registration Fee")
+  tag("td", tr, "$25")
+
+  // Go through all the fields in our saved data, and see if any of them have a cost associated with them
+  // If the field is one of the things we explicitly list a cost for (eg: meals) then grab that cost
+  for (const field in data) {
+    const costsForField = costs[field as keyof typeof costs]
+    if (costsForField?.is != data[field]) continue
+    subtotal += costsForField.cost
+    tr = tag("tr", subtotalElm)
+    tag("td", tr, field)
+    tag("td", tr, "$" + costsForField.cost)
+  }
+
+  // If the field is a workshop, then grab its cost
+  for (const field in data) {
+    const workshop = formStructure.Workshops[field] as WorkshopDetails
+    if (workshop == null || data[field] == false) continue
+    subtotal += workshop.totalCost
+    tr = tag("tr", subtotalElm)
+    tag("td", tr, workshop.title)
+    tag("td", tr, "$" + workshop.totalCost)
+  }
+
+  tr = tag("tr", subtotalElm)
+  tag("td", tr, "Total")
+  tag("td", tr, "$" + subtotal)
 }
 
 function fetchJson(url: string, json: any, method = "POST") {
-  return window.fetch(url, { method, body: JSON.stringify(json) })
+  return window.fetch(url, { method, body: JSON.stringify(json) }).then((res) => res.json())
 }
 
 function tag<K extends keyof HTMLElementTagNameMap>(
@@ -377,34 +450,34 @@ function tag<K extends keyof HTMLElementTagNameMap>(
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 const loading: HTMLElement = document.querySelector(".loading")!
-// const success: HTMLElement = document.querySelector(".success")!
-// const failure: HTMLElement = document.querySelector(".failure")!
+const subtotalElm: HTMLElement = document.querySelector(".subtotal")!
+const section = document.querySelector("section")!
 
-let id: string | null
+const url = new URL(window.location.href)
+const id = url.searchParams.get("id")
+
+let data: Record<WorkshopId, any> = {}
+let remaining: Record<WorkshopId, any> = {}
+
+// prettier-ignore
+function err(msg: string) {
+  loading.remove()
+  tag("h3", section, msg)
+  tag("p", section, "Something went wrong when loading your registration. Please return to the <a href='/'>Weaving Connections</a> home page and try registering again, or email <b>fulveland@gmail.com</b> for help.")
+}
 
 async function loadData() {
-  const url = new URL(window.location.href)
-  id = url.searchParams.get("id")
+  if (id == null) return err("Invalid Registration")
 
-  // Enable this in prod
-  // if (id == null) return console.log("No ID")
+  const res = await fetchJson("https://spiralganglion-weaving.web.val.run/load", { id })
 
-  let data: Record<string, string>
+  if (res.err) return err("Invalid Registration")
 
-  // For testing
-  if (id == null) {
-    data = {}
-  } else {
-    const res = await fetchJson("https://spiralganglion-weaving.web.val.run/load", { id })
-    data = await res.json()
-  }
-
-  // TODO: Handle the case where this ID is invalid (like, someone just typed random stuff in the URL)
-
-  // const data = {}
+  data = res.registration
+  remaining = res.remaining
 
   generateForm(formStructure, data)
-
+  updateSubtotal()
   loading.remove()
 }
 
